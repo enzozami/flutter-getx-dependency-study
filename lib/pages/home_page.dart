a2423ffb09b09a662e2c676c1bfe16f6a848d1ea
Enzo Zamineli <enzozamineli@gmail.com>
2025-09-16 14:33:50 -0300

    feat: bindings sem rota nomeada [nao é muito utilizado]

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get_dependencias/pages/bindings/bindings_controller.dart';
+import 'package:get_dependencias/pages/bindings/bindings_example.dart';
+import 'package:get_dependencias/pages/bindings/bindings_home_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -42,6 +48,16 @@ class HomePage extends StatelessWidget {
                 Navigator.of(context).pushNamed('/binding_builder_put');
               },
               child: Text('Binding Builder Put'),
+            ),
+            TextButton(
+              onPressed: () {
+                Get.to(
+                  BindingsHomePage(),
+                  binding:
+                      BindingsBuilder.put(() => BindingsController(nome: 'Rotas nao nomeadas')),
+                );
+              },
+              child: Text('Binding sem rota nomeada'),
             ),
           ],
         ),
